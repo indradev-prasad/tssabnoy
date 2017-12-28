03e7561dca7ac8c87500f5081b006eea7985533d
indradev prasad <kumarindradevd9211@gmail.com>
2017-12-28 22:12:36 +0530

    server.js

--- server.js
+++ server.js
@@ -4,8 +4,18 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
+//using https connect
+var fs = require('fs');
+var https = require('https');
+var options = {
+  key: fs.readFileSync('ssl_new/tssabnoy.key'),
+  cert: fs.readFileSync('ssl_new/tssabnoy_com.crt'),
+  ca: fs.readFileSync('ssl_new/tssabnoy_com.ca-bundle')
+};
+var http = https.createServer(options, app);
+//end here
 //socket configration===========================================================
-var http = require('http').Server(app);
+// var http = require('http').Server(app);
 var io = require('socket.io')(http);
 require('./app/socket')(io); // pass io for handling
 
