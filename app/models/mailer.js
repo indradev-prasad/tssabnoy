dec9b4c28fcd305d2c8b7b949e72351175acdce4
indradev prasad <kumarindradevd9211@gmail.com>
2018-01-02 00:51:07 +0530

    push

--- app/models/mailer.js
+++ app/models/mailer.js
@@ -14,8 +14,8 @@ module.exports = {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
             user: "indra19933@gmail.com", // generated ethereal user
             pass: "7352507926"  // generated ethereal password
