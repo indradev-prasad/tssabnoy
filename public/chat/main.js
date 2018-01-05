353fa1262a72ee2b5d8d7b69a82351d53a830cc3
indradev prasad <kumarindradevd9211@gmail.com>
2018-01-05 23:06:33 +0530

    push codde

--- public/chat/main.js
+++ public/chat/main.js
@@ -351,7 +351,10 @@ window.addEventListener('load', function() {
            document.querySelector('#conversation').setAttribute('style','max-height:'+y+'px;');
       if(x<1024){//phonne 
             document.querySelector('#conversation_section').setAttribute('style','height:'+y+'px;');
-            y=y-50;
+             y=y-50;//exclude header
+                if((x>480 &&x<700)){
+                     y=y-50;//exclude header
+                   }
             document.querySelector('#conversation').setAttribute('style','max-height:'+y+'px;');
       }
     }
